3cb3ad6b64ea1da1f3aadb20a8c9c53de72b872b
luclevert <77179446+luclevert@users.noreply.github.com>
2021-01-19 19:32:29 -0800

    fixed issues (game screen now loads)

--- Adventure/game.js
+++ Adventure/game.js
@@ -20,9 +20,10 @@ const gridBoxes = document.querySelectorAll("#gameBoard div");
 var currentLevel = 0; //starting level
 var riderOn = false; //is the rider on?
 var currentLocationOfRider = 0;
+var currentAnimation; //allows 1 animation per level
 
 //start game
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
   loadLevel();
 });
 
@@ -31,12 +32,84 @@ function loadLevel() {
   let levelMap = levels[currentLevel];
   let animateBoxes;
   riderOn = false;
-  console.log(gridBoxes.length);
 
   // load board
   for (var i = 0; i < gridBoxes.length; i++){
     gridBoxes[i].className = levelMap[i];
     if(levelMap[i].includes("riderup")) currentLocationOfRider = i;
   } // for
+  
+  animateBoxes = document.querySelectorAll(".animate");
+  
+  animateEnemy(animateBoxes, 0, "right");
 
 } // loadLevel
+
+// animate enemy left to right (could add up and down)
+// boxes - array of grid boxes that include animation
+// index - current location of animation
+// direction - current direction of animation
+function animateEnemy(boxes, index, direction) {
+	
+	// exit function if no animation
+	if (boxes.length <= 0) { return; }
+	
+	// update images
+	if (direction == "right"){
+	  boxes[index].classList.add("enemyright");
+	} else if (direction == "left"){
+	  boxes[index].classList.add("enemyleft");
+	} else if (direction == "up"){
+	  boxes[index].classList.add("enemyup");
+	} else if (direction == "down"){
+	  boxes[index].classList.add("enemydown");
+	}
+	
+	//remove images from other boxes
+	for(var i = 0; i < boxes.length; i++){
+	  if(i != index){
+		boxes[i].classList.remove("enemyleft");
+		boxes[i].classList.remove("enemyright");
+		boxes[i].classList.remove("enemyup");
+		boxes[i].classList.remove("enemydown");
+	  } // if
+	} // for
+	
+	//moving right
+	if (direction == "right") {
+		
+		// turn around if hit right side
+		if (index == boxes.length - 1) {
+		  index--;
+		  direction = "left";
+		} else {
+		  index++;
+		}
+	} else if (direction == "left"){
+	  if (index == 0) {
+		index++;
+		direction = "right";
+	  } else {
+		index--;
+	  }
+	} else if (direction == "up") {
+	  if(index == 0) {
+	    index += 5;
+		direction = "down";
+	  } else {
+		index -= 5;
+	  } 
+	} else if (direction == "down") {
+		if(index == 0){
+		  index -= 5;
+		  direction = "up";
+		} else {
+		  index += 5;
+		}  
+	}//else if
+	
+	currentAnimation = setTimeout(function() {
+	  animateEnemy(boxes, index, direction);
+	}, 750);
+	  
+}// animate enemy
